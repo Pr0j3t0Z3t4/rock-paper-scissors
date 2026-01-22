c9415102808c28c8dfca3024919ffe0e39bf7a43
Pr0j3t0Z3t4 <lukaxnascimento@yahoo.com.br>
2026-01-22 00:09:24 -0300

    feat: setup user input collection and parsing

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+const start = ["Rock", "Paper", "Scissors"]
+
+let input = prompt(" || 1 = Rock || 2 = Paper || 3 = Scissors || ")
+let humanPick = parseInt(input)
+
 const options = ["Rock", "Paper", "Scissors"];
 function getComputerChoice() {
     const randomIndex = Math.floor(Math.random() * options.length);
     return options[randomIndex];
 }
-console.log(getComputerChoice());
+
+console.log(humanPick);
